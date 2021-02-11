a32cf7daf7e881ad928fdb098507d05c35d6c1b5
Student <pbjunkiep3@yahoo.com>
2021-02-11 16:09:51 +1100

    added build

---
+++ build/precache-manifest.e3302d4e22ad3cfab6d1c6fb113648eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49f17372334ad8b14759d3f319ac805f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0cc552110003ffd0883c",
+    "url": "/static/css/main.3f65c319.chunk.css"
+  },
+  {
+    "revision": "3374af0def0143b8c9da",
+    "url": "/static/js/2.b777960a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b777960a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cc552110003ffd0883c",
+    "url": "/static/js/main.1f1e5c96.chunk.js"
+  },
+  {
+    "revision": "68046c7f557a5d27053f",
+    "url": "/static/js/runtime-main.f2a18a3d.js"
+  }
+]);
